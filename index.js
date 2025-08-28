e4363229dd42a6bbb62d050e780aa0b2d639c050
Usmaan Bajwa <ukbajwa10@gmail.com>
2025-08-27 22:16:48 -0500

    redesigned resume site, added all css to index

--- index.js
+++
@@ -1,7 +0,0 @@
-var counterContainer = document.querySelector(".website-counter");
-if (counterContainer) {
-    var visitCount = localStorage.getItem("page_view") || 0;
-    visitCount = Number(visitCount) + 1;
-    localStorage.setItem("page_view", visitCount);
-    counterContainer.innerHTML = visitCount;
-}
